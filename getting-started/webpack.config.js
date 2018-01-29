33ef354f4298f7182d9d359b463b74fa48a9c147
kanghonggu <wckhg89@gmail.com>
2018-01-29 18:27:26 +0900

    Strat webpack

---
+++ getting-started/webpack.config.js
@@ -0,0 +1,11 @@
+// webpack.config.js
+// `webpack` command will pick up this config setup by default
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
